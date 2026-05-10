c88b90231bb0de8c1037de1dcd59f46f10ee0755
TheBestPlant <savannahbsf@gmail.com>
2026-05-09 22:24:27 -0700

    RAAAAHHH INTERLINKED WORKS

--- js/main.js
+++ js/main.js
@@ -300,3 +300,35 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
 });
+
+// =========================
+// FULLSCREEN
+// =========================
+
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("modalImage");
+const images = document.querySelectorAll(".blueprint-image");
+const closeBtn = document.querySelector(".close-modal");
+
+images.forEach(img => {
+    img.addEventListener("click", () => {
+        modal.style.display = "block";
+        modalImg.src = img.src;
+    });
+});
+
+closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        modal.style.display = "none";
+    }
+});
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+        modal.style.display = "none";
+    }
+});
